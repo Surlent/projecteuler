f453361d2dd4cb80b5a3601df5b2c2a037fcdff8
ssaporito <sandrosaporito@outlook.com>
2015-03-31 22:00:46 -0300

    after 11

--- functions.js
+++ functions.js
@@ -1,3 +1,10 @@
+function format_time(time)
+{
+	var split_time=time.toString().split(".");
+	var significand=split_time[0].substr(0,12);
+	var mantissa=split_time[1].substr(0,6);
+	return significand+","+mantissa+"ms";
+}
 function is_prime(n)
 {
 	var sqrtN=Math.sqrt(n);
@@ -263,3 +270,86 @@ function sum_primes(n)
 	return sum;
 }
 // End of Number 10
+
+//Number 11
+function highest_product_grid(matrix,n)
+{
+	var highest_product=0;
+	var best_indexes=[];
+	for(var i=0;i<matrix.length;i++)
+	{
+		for(var j=0;j<matrix[i].length;j++)
+		{
+			if(i+n-1<matrix.length)
+			{
+				var test_product=1;
+				for(var k=0;k<n;k++)
+				{
+					test_product*=matrix[i+k][j];
+				}
+				if(highest_product<test_product)
+				{
+					highest_product=test_product;
+					best_indexes=[];
+					for(var k=0;k<n;k++)
+					{
+						best_indexes.push([matrix[i+k][j],i+k,j]);
+					}
+				}
+				if(j+n-1<matrix[i].length)
+				{
+					var test_product=1;
+					for(var k=0;k<n;k++)
+					{
+						test_product*=matrix[i+k][j+k];
+					}
+					if(highest_product<test_product)
+					{
+						highest_product=test_product;
+						best_indexes=[];
+						for(var k=0;k<n;k++)
+						{
+							best_indexes.push([matrix[i+k][j+k],i+k,j+k]);
+						}
+					}
+				}
+				if(j-(n-1)>=0)
+				{
+					var test_product=1;
+					for(var k=0;k<n;k++)
+					{
+						test_product*=matrix[i+k][j-k];
+					}
+					if(highest_product<test_product)
+					{
+						highest_product=test_product;
+						best_indexes=[];
+						for(var k=0;k<n;k++)
+						{
+							best_indexes.push([matrix[i+k][j-k],i+k,j-k]);
+						}
+					}
+				}
+			}
+			if(j+n-1<matrix[i].length)
+			{
+				var test_product=1;
+				for(var k=0;k<n;k++)
+				{
+					test_product*=matrix[i][j+k];
+				}
+				if(highest_product<test_product)
+				{
+					highest_product=test_product;
+					best_indexes=[];
+					for(var k=0;k<n;k++)
+					{
+						best_indexes.push([matrix[i][j+k],i,j+k]);
+					}
+				}
+			}
+		}
+	}
+	return [highest_product,best_indexes];
+}
+// End of Number 11
