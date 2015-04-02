c29b1942e1a1735d3e4f4ca296bcf3982445da7f
ssaporito <sandrosaporito@outlook.com>
2015-04-01 22:59:42 -0300

    optimized 91

--- PE-091.js
+++ PE-091.js
@@ -1,5 +1,9 @@
 function count_right_triangles(n)
 {
+	return count_right_triangles_v1(n);
+}
+function count_right_triangles_v1(n)
+{
 	var m=n+1;
 	//var linear_grid = new Array(n*n);
 	var m_sqr=m*m;
@@ -25,37 +29,28 @@ function count_right_triangles_v2(n)
 {
 	var m=n+1;
 	//var linear_grid = new Array(n*n);
-	var m_sqr=m*m;
+	//var m_sqr=m*m;
 	var count=0;
 	var list=[];
 	for(var k1=0;k1<m;k1++)
 	{
 		for(var k2=0;k2<m;k2++)
-		{		
-			if(k1!=0||k2!=0)
-			{	
-				for(var l1=0;l1<m;l1++)
+		{			
+			for(var l1=0;l1<m;l1++)
+			{
+				for(var l2=0;l2<m;l2++)
 				{
-					for(var l2=0;l2<m;l2++)
+					var p1=[k1,k2];
+					var p2=[l1,l2];
+					//console.log([[0,0],p1,p2])
+					if (is_right_triangle([0,0],p1,p2))
 					{
-						if(l1!=0||l2!=0)
-						{
-							if (k1!=l1||k2!=l2)
-							{
-								var p1=[k1,k2];
-								var p2=[l1,l2];
-								//console.log([[0,0],p1,p2])
-								if (is_right_triangle([0,0],p1,p2))
-								{
-									count++;
-								}
-							}							
-						}						
-					}	
-				}
-			}
+						count++;
+					}											
+				}	
+			}		
 		}
 	}
 	//console.log(list);
-	return count/2;
+	return count;
 }
--- functions.js
+++ functions.js
@@ -1,9 +1,13 @@
 function format_time(time)
 {
 	var split_time=time.toString().split(".");
-	var significand=split_time[0].substr(0,12);
-	var mantissa=split_time[1].substr(0,6);
-	return significand+","+mantissa+"ms";
+	if(split_time.length>1)
+	{
+		var significand=split_time[0].substr(0,12);
+		var mantissa=split_time[1].substr(0,6);
+		return significand+","+mantissa+"ms";
+	}
+	else return split_time+"ms";
 }
 function is_prime(n)
 {
@@ -45,26 +49,22 @@ function primes_list(n)
 }
 function dot_product(a,b)
 {
-   if(a.length=b.length)
-   {
-   		var dot=0;
+   		var dot=0;		
    		for(k=0;k<a.length;k++)
    		{
    			dot+=a[k]*b[k];
    		}
    		return dot;
-   }
-   else
-   {
-   	   return false;
-   	   console.log("Vectors have different size.");
-   }
 }
 function is_right_triangle(p1,p2,p3)
+{
+	return is_right_triangle_v2(p1,p2,p3);
+}
+function is_right_triangle_v1(p1,p2,p3)
 {	
 	var v1=[p1[0]-p3[0],p1[1]-p3[1]];
 	var v2=[p2[0]-p3[0],p2[1]-p3[1]];
-	var v3=[p2[0]-p1[0],p2[1]-p1[1]];
+	var v3=[p2[0]-p1[0],p2[1]-p1[1]];	
 	if((dot_product(v1,v2)==0)||(dot_product(v1,v3)==0)||(dot_product(v2,v3)==0))
 	{
 		return true;
@@ -72,5 +72,24 @@ function is_right_triangle(p1,p2,p3)
 	else
 	{
 		return false;
+	}
+}
+function is_right_triangle_v2(p1,p2,p3)
+{
+	var sides=new Array(3);
+	sides[0]=((p1[0]-p3[0])*(p1[0]-p3[0])+(p1[1]-p3[1])*(p1[1]-p3[1]));
+	sides[1]=((p2[0]-p3[0])*(p2[0]-p3[0])+(p2[1]-p3[1])*(p2[1]-p3[1]));
+	sides[2]=((p2[0]-p1[0])*(p2[0]-p1[0])+(p2[1]-p1[1])*(p2[1]-p1[1]));
+	if(sides[0]>sides[1]&&sides[0]>sides[2])
+	{
+		return (sides[0]==sides[1]+sides[2]);
+	}
+	else if(sides[1]>sides[0]&&sides[1]>sides[2])
+	{
+		return (sides[1]==sides[0]+sides[2]);
+	}
+	else
+	{
+		return (sides[2]==sides[0]+sides[1]);
 	}
 }
