b24dc752f19fc4208e3aa7101f457003a26473d0
ssaporito <sandrosaporito@outlook.com>
2015-04-06 21:53:46 -0300

    after 14

--- PE-014.js
+++ PE-014.js
@@ -3,30 +3,33 @@ function longest_collatz_sequence(n)
 	var longest_sequence=[];
 	var longest_count=0;
 	var starting_number=0;
-	var reached_numbers=[];
+	var reached_numbers={};
 	for(var i=1;i<n;i++)
-	{
-		var current=i;		
-		if(!(current in reached_numbers))
-		{
-			var current_count=1;		
-			var current_sequence=[];
-			current_sequence.push(current);
-			while (current!=1)
+	{				
+			if(!(i in reached_numbers))
 			{
-				reached_numbers.push(current);
-				current=collatz_next(current);
-				current_count++;
+				var current=i;	
+				var current_count=1;		
+				var current_sequence=[];			
 				current_sequence.push(current);
+				//var k=0;
+				while (current!==1)
+				{											
+					current=collatz_next(current);
+					current_count++;
+					current_sequence.push(current);
+					reached_numbers[current]=1;
+					//k++;			
+				}
+				if(current_count>longest_count)//current_sequence.length>longest_sequence.length)
+				{	
+					longest_count=current_count;
+					starting_number=i;
+					longest_sequence=current_sequence;			
+				}
 			}
-			if(current_count>longest_count)//current_sequence.length>longest_sequence.length)
-			{	
-				longest_count=current_count;
-				starting_number=i;
-				longest_sequence=current_sequence;			
-			}
-		}
 	}
+	//console.log(reached_numbers)
 	console.log(longest_sequence);
 	console.log(longest_count);
 	return starting_number;
