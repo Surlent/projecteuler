f3be83d8db4af315981c2bff51a39971b9249c4d
ssaporito <sandrosaporito@outlook.com>
2015-04-07 23:51:34 -0300

    after 18

--- PE-018.js
+++ PE-018.js
@@ -1,6 +1,5 @@
 function path_sum(triangle,n,i)
 {	
-	//console.log("n:"+n+",i:"+i+",number:"+triangle[n][i]);
 	var increase=0;
 	if(n==1){
 			increase=triangle[0][0];
@@ -9,17 +8,13 @@ function path_sum(triangle,n,i)
 	{
 			increase=path_sum(triangle,n-1,i);
 	}
-	else if((i==1)||(i==n-1))
-	{
-			increase=Math.max(path_sum(triangle,n-1,i-1),path_sum(triangle,n-1,i));
-	}
 	else if(i==n)
 	{
 			increase=path_sum(triangle,n-1,i-1);
 	}
 	else
 	{
-			increase=Math.max(path_sum(triangle,n-1,i-1),path_sum(triangle,n-1,i),path_sum(triangle,n-1,i+1));
+			increase=Math.max(path_sum(triangle,n-1,i-1),path_sum(triangle,n-1,i));
 	}
 	return triangle[n][i]+increase;
 }
@@ -31,30 +26,16 @@ function path(triangle,n,i)
 	}
 	else if(i==0)
 	{
-			//increase=path(triangle,n-1,i);
 			chosen_index=i;
-	}
-	else if((i==1)||(i==n-1))
-	{
-			//increase=Math.max(path(triangle,n-1,i-1),path_sum(triangle,n-1,i));
-			if(path_sum(triangle,n-1,i)>path_sum(triangle,n-1,i-1))
-			{
-				chosen_index=i;
-			}
-			else
-			{
-				chosen_index=i-1;
-			}
 	}
 	else if(i==n)
 	{
-			//increase=path_sum(triangle,n-1,i-1);
 			chosen_index=i-1;
 	}
 	else
 	{
-			increase=0;//Math.max(path_sum(triangle,n-1,i-1),path_sum(triangle,n-1,i),path_sum(triangle,n-1,i+1));
-			for(var k=-1;k<=1;k++)
+			increase=0;
+			for(var k=-1;k<=0;k++)
 			{
 				var sum=path_sum(triangle,n-1,i+k);
 				if(sum>increase)
@@ -64,7 +45,7 @@ function path(triangle,n,i)
 				}
 			}
 	}
-	console.log("n:"+n+",i:"+i);
+	//console.log("n:"+n+",i:"+i);
 	return [[n,i,triangle[n][i]]].concat(path(triangle,n-1,chosen_index));
 }
 function max_path_sum(triangle)
@@ -83,6 +64,6 @@ function max_path_sum(triangle)
 	}
 	//var path1=path(triangle,last_pair[0],last_pair[1]);
 	//console.log(path1.reduce(function(a,b){console.log("a:"+a[2]+",b:"+b[2]);return a+b[2];},0));
-	//console.log(path(last_pair[0],last_pair[1]));
+	//console.log(path1);
 	return max_sum;
 }
